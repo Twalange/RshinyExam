132fc0e1cc0f1912f9491bf18b0d5d24e99231d0
Twalange <boiteau.yvan@gmail.com>
2025-02-22 18:41:12 +0100

    Front end: sidebar + main panel

--- app.R
+++ app.R
@@ -1,6 +1,46 @@
 library(shiny)
+library(tidyverse)
+library(DT)
+library(bslib)
+library(thematic)
 
-ui <- fluidPage()
+ui <- fluidPage(
+  theme = bs_theme(
+    version = 5,
+    bootswatch = "minty"
+  ),
+  h2("Exploration des Diamants"),
+  sidebarLayout(
+    sidebarPanel(
+      radioButtons(
+        inputId = "shouldScatterPlotBePink",
+        label = "Colorier les points en rose ?",
+        choices = c("Oui", "Non")
+      ),
+      selectInput(
+        inputId = "colorFilter",
+        label = "Choisir une couleur à filtrer :",
+        choices = levels(diamonds$color)
+      ),
+      sliderInput(
+        inputId = "priceFilter",
+        label = "Prix maximum :",
+        min = 300,
+        max = 20000,
+        value = 5000
+      ),
+      actionButton(
+        inputId = "showNotification",
+        label = "Afficher une notification",
+      )
+    ),
+    mainPanel(
+      textOutput("textFilters"),
+      plotOutput("diamondPlot"),
+      DT::DTOutput("DTOutput")
+    )
+  )
+)
 
 server <- function(input, output) {}
 
